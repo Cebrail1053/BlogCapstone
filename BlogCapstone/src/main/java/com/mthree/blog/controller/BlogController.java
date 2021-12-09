d3da2557699e1f846cd1014495c44b425145f2bd
delaguardianick <delaguardianick@gmail.com>
2021-12-09 10:50:38 -0500

    added endpoints

--- BlogCapstone/src/main/java/com/mthree/blog/controller/BlogController.java
+++ BlogCapstone/src/main/java/com/mthree/blog/controller/BlogController.java
@@ -1,10 +1,16 @@
 package com.mthree.blog.controller;
 
+import com.mthree.blog.entities.Post;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.mthree.blog.service.BlogServiceLayer;
+import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @RequestMapping("/api")
@@ -14,5 +20,26 @@ public class BlogController {
 	private BlogServiceLayer svc;
 	
 	//TODO: Implement REST endpoints
+        
+        
+    @PostMapping("/post")
+    public int createPost(@RequestBody Post newPost){
+        return 1;
+    }
+    
+    @GetMapping("/post/{postID}")
+    public Post getPost(@PathVariable int postID){
+        return null;
+    }
+    
+    @GetMapping("/posts")
+    public List<Post> getAllPosts(){
+        return null;
+    }
+    
+    @GetMapping("/categories")
+    public List<Category> getAllCategories(){
+        return null;
+    }
 
 }
